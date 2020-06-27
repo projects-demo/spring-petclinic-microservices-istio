060bb6848672025420286c271c4146e86b9d7259
jdk-world <saurabh2204@gmail.com>
2020-06-27 23:33:53 +0530

    updates

--- spring-petclinic-customers-service-istio/src/main/java/com/pnm/kube/kube/HelloKube.java
+++ spring-petclinic-customers-service-istio/src/main/java/com/pnm/kube/kube/HelloKube.java
@@ -3,6 +3,7 @@ package com.pnm.kube.kube;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,8 +16,10 @@ public class HelloKube {
 	public String welcome() {
 		String message = "response from customer-service";
 		try {
+			System.err.println("1Hello in welcome");
 			message = restTemplate.getForObject("http://vets-service:8888/hello", String.class)
 					+ "vets-service response";
+			System.err.println("2Hello in welcome");
 
 		} catch (Exception e) {
 			try {
@@ -24,13 +27,30 @@ public class HelloKube {
 				System.out.println("1 Exception " + e.getMessage());
 
 				message = restTemplate.getForObject("http://localhost:8888/hello", String.class) + "localhost response";
+				System.out.println("3 Exception " + e.getMessage());
+
 			} catch (Exception e2) {
 				System.out.println("2 Exception " + e2.getMessage());
 			}
 		}
+		System.out.println("5 Exception " );
+
 		return message;
 	}
 
+	@RequestMapping("/dear")
+	@ResponseBody
+	public String welcome1() {
+		return "dear.....";
+	}
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String welcome2() {
+		return "2dear.....";
+	}
+
+	
 	@Bean
 	public RestTemplate rest() {
 		return new RestTemplate();
